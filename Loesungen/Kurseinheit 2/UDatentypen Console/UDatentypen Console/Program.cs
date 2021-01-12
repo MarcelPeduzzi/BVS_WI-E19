cfdb60725db0e4b10d9f3e3c78ed2ad6ecbd0841
Marcel Peduzzi <marcel.peduzzi@hotmail.com>
2021-01-12 18:37:26 +0100

    Verschiebung Uebungen Kurseinheit 3

---
+++ Loesungen/Kurseinheit 2/UDatentypen Console/UDatentypen Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDatentypen_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Marcel";
+            string lastName = "Peduzzi";
+            string street = "Dorfstrasse 21";
+            string plz = "9306";
+            string location = "Freidorf";
+            int age = 49;
+            decimal salary = 5000;
+
+            Console.WriteLine("Adresse:");
+            Console.WriteLine(firstName + " " + lastName);
+            Console.WriteLine(street);
+            Console.WriteLine(plz + " " + location);
+            Console.WriteLine();
+            Console.WriteLine("Alter: " + age);
+            Console.WriteLine("Gehalt: " + salary);
+
+            Console.ReadKey(false);
+        }
+    }
+}
