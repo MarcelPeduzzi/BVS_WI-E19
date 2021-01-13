42b9b08f11da19d33c47bb57b0ebd0d7bdda83de
Marcel Peduzzi <marcel.peduzzi@hotmail.com>
2021-01-13 19:30:34 +0100

    Beispiel Arithmetische Operatoren

---
+++ Beispiele/New folder/ArithmetischeOperatoren/ArithmetischeOperatoren/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmetischeOperatoren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 7;
+
+            int c = a + b;
+            Console.WriteLine("a + b = " + c);
+
+            c = a - b;
+            Console.WriteLine("a - b = " + c);
+
+            c = a / b;
+            Console.WriteLine("a / b = " + c); // liefert 1 da in einem int keine Nachkommastellen vorhanden sind
+
+            c = a * b;
+            Console.WriteLine("a * b = " + c);
+
+            c = a % b;
+            Console.WriteLine("a % b = " + c); // Module = Restwert
+
+
+            Console.ReadKey(false);
+        }
+    }
+}
